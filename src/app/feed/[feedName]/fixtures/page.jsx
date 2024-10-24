8d19168bc5828ea75c933e81102514fc25054232
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2024-10-24 17:53:18 +0300

    düzenleme

--- src/app/feed/[feedName]/fixtures/page.jsx
+++ src/app/feed/[feedName]/fixtures/page.jsx
@@ -37,13 +37,13 @@ function Page({ params }) {
     ];
 
     return (
-        <div className="px-24 max-sm:px-0 py-8 max-sm:py-3 w-full h-full">
+        <div className="px-24 max-sm:pb-24 max-sm:px-0 py-8 max-sm:py-3 w-full h-full overflow-auto">
             <div className="max-w-[75rem] flex space-x-4 mb-4 px-4 py-3 rounded-lg bg-gray-100">
-                <Link href={`/feed/${feedName}/standings`} className="text-lg  max-sm:text-[12px] flex items-center justify-between gap-4 font-semibold text-gray-600 rounded-lg text-start">
+                <Link href={`/feed/${feedName}/standings`} className="text-lg  max-sm:text-[15px] flex items-center justify-between gap-4 font-semibold text-gray-600 rounded-lg text-start">
                     Puan Durumu
                 </Link>
-                <h1 className="text-lg max-sm:text-[12px] flex items-center justify-between gap-4 font-semibold text-indigo-600 rounded-lg bg-gray-100 text-start">Fikstür</h1>
-                <Link href={`/feed/${feedName}/topScorers`} className="text-lg  max-sm:text-[12px] flex items-center justify-between gap-4 font-semibold text-gray-600 text-start">
+                <h1 className="text-lg max-sm:text-[15px] flex items-center justify-between gap-4 font-semibold text-indigo-600 rounded-lg bg-gray-100 text-start">Fikstür</h1>
+                <Link href={`/feed/${feedName}/topScorers`} className="text-lg  max-sm:text-[15px] flex items-center justify-between gap-4 font-semibold text-gray-600 text-start">
                     Gol Krallığı
                 </Link>
             </div>
@@ -53,7 +53,7 @@ function Page({ params }) {
                     <button
                         key={league.key}
                         onClick={() => handleLeagueClick(league.key)}
-                        className={`flex-grow min-w-[5rem]  max-sm:text-[12px] max-w-[8rem] py-2 border text-center transition-all border-gray-200 rounded-lg font-semibold text-[12px] flex items-center justify-center gap-1 text-gray-500 ${
+                        className={`flex-grow min-w-[5rem]  max-sm:text-[15px] max-w-[8rem] py-2 border text-center transition-all border-gray-200 rounded-lg font-semibold text-[12px] flex items-center justify-center gap-1 text-gray-500 ${
                             selectedLeague === league.key ? "transition-all bg-indigo-500 text-white" : "bg-white"
                         }`}
                     >
