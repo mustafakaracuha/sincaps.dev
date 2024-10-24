d37a175fbb3d4aae963e7ddfe3dcb7b5b5a2b69a
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2024-10-24 15:56:57 +0300

    düzenleme

--- src/app/page.js
+++ src/app/page.js
@@ -11,13 +11,13 @@ import { GiMedicines } from "react-icons/gi";
 
 function Page() {
     const newsTopics = [
-        { link: "guncel", title: "Son Dakika", description: "En son haberleri ve gelişmeleri keşfedin.", icon: <FaNewspaper className="text-red-500 text-lg" /> },
-        { link: "teknoloji", title: "Teknoloji", description: "Gündemdeki en son teknoloji gelişmeleri.", icon: <FaLaptopCode className="text-yellow-500 text-lg" /> },
-        { link: "egitim", title: "Eğitim", description: "Eğitimde son durum ve önemli gelişmeler.", icon: <FaChartLine className="text-pink-500 text-lg" /> },
-        { link: "saglik", title: "Sağlık", description: "Sağlık alanındaki yenilikler ve bilgiler.", icon: <FaHeartbeat className="text-orange-500 text-lg" /> },
-        { link: "spor", title: "Spor", description: "Spor dünyasından haberler ve etkinlikler.", icon: <FaFutbol className="text-green-500 text-lg" /> },
-        { link: "dini-bilgiler", title: "Dini Bilgiler", description: "Dini konularda güncel bilgiler ve kaynaklar.", icon: <FaBook className="text-blue-500 text-lg" /> },
-        { link: "magazin", title: "Magazin", description: "Magazin dünyasından haberler.", icon: <FaFilm className="text-gray-500 text-lg" /> },
+        { link: "guncel", title: "Son Dakika", description: "En son haberleri ve gelişmeleri keşfedin.", icon: <FaNewspaper className="text-red-500" /> },
+        { link: "teknoloji", title: "Teknoloji", description: "Gündemdeki en son teknoloji gelişmeleri.", icon: <FaLaptopCode className="text-yellow-500" /> },
+        { link: "egitim", title: "Eğitim", description: "Eğitimde son durum ve önemli gelişmeler.", icon: <FaChartLine className="text-pink-500" /> },
+        { link: "saglik", title: "Sağlık", description: "Sağlık alanındaki yenilikler ve bilgiler.", icon: <FaHeartbeat className="text-orange-500" /> },
+        { link: "spor", title: "Spor", description: "Spor dünyasından haberler ve etkinlikler.", icon: <FaFutbol className="text-green-500" /> },
+        { link: "dini-bilgiler", title: "Dini Bilgiler", description: "Dini konularda güncel bilgiler ve kaynaklar.", icon: <FaBook className="text-blue-500" /> },
+        { link: "magazin", title: "Magazin", description: "Magazin dünyasından haberler.", icon: <FaFilm className="text-gray-500" /> },
     ];
 
     const toolTopics = [
@@ -25,16 +25,16 @@ function Page() {
             link: "deprem",
             title: "Deprem",
             description: "Deprem anında nasıl hareket etmeniz gerektiği hakkında",
-            icon: <RiEarthquakeFill className="text-fuchsia-500 text-[19px]" />,
+            icon: <RiEarthquakeFill className="text-fuchsia-500" />,
             path: "/earthquake",
         },
-        { link: "sondepremler", title: "Son Depremler", description: "En yakın zamanda olan depremler", icon: <WiEarthquake className="text-indigo-500 text-[22px]" />, path: "/recent-earthquakes" },
-        { link: "nobetcieczane", title: "Nöbetçi Eczaneler", description: "Şehrinizdeki nöbetçi eczaneler", icon: <GiMedicines className="text-yellow-500 text-[22px]" />, path: "/pharmacies-oncall" },
-        { link: "trafikcezasi", title: "Trafik Cezaları", description: "2024 yılı trafik cezaları", icon: <FaCarCrash className="text-red-500 text-xl" />, path: "/traffic-fine" },
-        { link: "trafikkurallari", title: "Trafik Kuralları", description: "Trafik kuralları", icon: <PiTrafficSignFill className="text-gray-400 text-[19px]" />, path: "/traffic-rules" },
-        { link: "cevreyikorumak", title: "Çevreyi Korumak", description: "Çevreyi korumak hakkında", icon: <FaTree className="text-lime-600 text-[18px]" />, path: "/protecting-environment" },
-        { link: "hayvanlarikorumak", title: "Minik Dostlar", description: "Hayvanları koruma yolları ve yapılabilecekler hakkında bilgi edinin", icon: <FaPaw className="text-teal-500 text-[15px]" />, path: "/protecting-animals" },
-        { link: "kisiselbakim", title: "Kişisel Bakım", description: "Sağlıklı ve bakımlı görünmek için öneriler ve ipuçları", icon: <FaSpa className="text-pink-400 text-[16px]" />, path: "/personal-care" },
+        { link: "sondepremler", title: "Son Depremler", description: "En yakın zamanda olan depremler", icon: <WiEarthquake className="text-indigo-500" />, path: "/recent-earthquakes" },
+        { link: "nobetcieczane", title: "Nöbetçi Eczaneler", description: "Şehrinizdeki nöbetçi eczaneler", icon: <GiMedicines className="text-yellow-500" />, path: "/pharmacies-oncall" },
+        { link: "trafikcezasi", title: "Trafik Cezaları", description: "2024 yılı trafik cezaları", icon: <FaCarCrash className="text-red-500" />, path: "/traffic-fine" },
+        { link: "trafikkurallari", title: "Trafik Kuralları", description: "Trafik kuralları", icon: <PiTrafficSignFill className="text-gray-400" />, path: "/traffic-rules" },
+        { link: "cevreyikorumak", title: "Çevreyi Korumak", description: "Çevreyi korumak hakkında", icon: <FaTree className="text-lime-600" />, path: "/protecting-environment" },
+        { link: "hayvanlarikorumak", title: "Minik Dostlar", description: "Hayvanları koruma yolları ve yapılabilecekler hakkında bilgi edinin", icon: <FaPaw className="text-teal-500" />, path: "/protecting-animals" },
+        { link: "kisiselbakim", title: "Kişisel Bakım", description: "Sağlıklı ve bakımlı görünmek için öneriler ve ipuçları", icon: <FaSpa className="text-pink-400" />, path: "/personal-care" },
     ];
 
     return (
@@ -62,7 +62,7 @@ function Page() {
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: 0.3 }}
-                className="text-[16px] max-sm:text-[14px] font-semibold text-gray-600 mt-3 mb-6"
+                className="text-[16px] max-sm:text-[16px] font-semibold text-gray-600 mt-3 mb-6"
             >
                 Bilgilendirme
             </motion.h2>
@@ -76,11 +76,11 @@ function Page() {
                         transition={{ duration: 0.3, delay: index * 0.1 }}
                     >
                         <div className="w-full flex items-center justify-between mb-5">
-                            <h3 className="text-[13px] max-sm:text-[13px] font-semibold text-gray-600">{topic.title}</h3>
-                            <div>{topic.icon}</div>
+                            <h3 className="text-[13px] max-sm:text-[15px] font-semibold text-gray-600">{topic.title}</h3>
+                            <div className="text-[17px] max-sm:text-[23px]">{topic.icon}</div>
                         </div>
-                        <p className="text-gray-400 max-sm:text-[12px] text-[13px] mt-2 mb-4">{topic.description}</p>
-                        <Link href={topic.path} className="text-indigo-500 max-sm:text-[12px] text-[14px] font-medium hover:underline">
+                        <p className="text-gray-400 max-sm:text-[14px] text-[13px] mt-2 mb-4">{topic.description}</p>
+                        <Link href={topic.path} className="text-indigo-500 text-[13px] font-semibold hover:underline">
                             oku
                         </Link>
                     </motion.div>
@@ -90,7 +90,7 @@ function Page() {
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: 0.3 }}
-                className="text-[16px] max-sm:text-[14px] font-semibold text-gray-600 mt-12 mb-6"
+                className="text-[16px] max-sm:text-[16px] font-semibold text-gray-600 mt-12 mb-6"
             >
                 Haberler
             </motion.h2>
@@ -104,11 +104,11 @@ function Page() {
                         transition={{ duration: 0.3, delay: index * 0.2 }}
                     >
                         <div className="w-full flex items-center justify-between mb-5">
-                            <h3 className="text-[13px] max-sm:text-[13px] font-semibold text-gray-600">{topic.title}</h3>
-                            <div>{topic.icon}</div>
+                            <h3 className="text-[13px] max-sm:text-[15px] font-semibold text-gray-600">{topic.title}</h3>
+                            <div className="text-[17px] max-sm:text-[23px]">{topic.icon}</div>
                         </div>
-                        <p className="text-gray-400 max-sm:text-[12px] text-[13px] mt-2 mb-4">{topic.description}</p>
-                        <Link href={`/feed/${topic.link}`} className="text-indigo-500 max-sm:text-[12px] text-[14px] font-medium hover:underline">
+                        <p className="text-gray-400 max-sm:text-[14px] text-[13px] mt-2 mb-4">{topic.description}</p>
+                        <Link href={`/feed/${topic.link}`} className="text-indigo-500 text-[13px] font-semibold hover:underline">
                             incele
                         </Link>
                     </motion.div>
